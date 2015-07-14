36c29ef3d003ef21500bb94e4dc7c40c2720b9a9
Dan Seethaler <dan@seethalers.com>
2015-07-14 06:35:30 -0600

    Response user data now being stored.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var FS = require('familysearch-javascript-sdk'),
+var FamilySearch = require('familysearch-javascript-sdk'),
 	request = require('request'),
 	q = require('q'),
+	fs = require('fs'),
 	express = require('express'),
 	bodyParser = require("body-parser");
 
@@ -16,7 +17,7 @@ app.post('/signedOn', function (req, res) {
 
 	console.log(req.body.accessToken);
 
-	var client = new FS({
+	var client = new FamilySearch({
 		client_id: 'a02j0000007rShWAAU',
 		environment: 'sandbox',
 		access_token: req.body.accessToken,
@@ -26,8 +27,26 @@ app.post('/signedOn', function (req, res) {
 
 	client.getCurrentUser().then(function (response) {
 		var user = response.getUser();
-		console.log('Hellooo0o ');
-		console.log(user);
+
+		var thisUser = {};
+
+		thisUser.contactName = user.contactName;
+		thisUser.helperAccessPin = user.helperAccessPin;
+		thisUser.givenName = user.givenName;
+		thisUser.familyName = user.familyName;
+		thisUser.email = user.email;
+		thisUser.country = user.country;
+		thisUser.gender = user.gender;
+		thisUser.birthDate = user.birthDate;
+		thisUser.preferredLanguage = user.preferredLanguage;
+		thisUser.displayName = user.displayName;
+		thisUser.personId = user.personId;
+		thisUser.treeUserId	= user.treeUserId;
+
+		fs.writeFile('user.json', JSON.stringify(thisUser, null, 4), function (err) {
+			console.log('File successfully written.');
+		})
+
 	});
 });
 
